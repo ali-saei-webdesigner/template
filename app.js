a763d10cacf0f9a16beca95a1305b670a15909b3
ali saei <ali.saei.webdesign@gmail.com>
2024-10-26 18:45:09 +0330

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+
+   
+        document.getElementById('button1').addEventListener('click', function() {
+            window.scrollTo({
+                top: document.body.scrollHeight,
+                behavior: 'smooth'
+            });
+        });
+
